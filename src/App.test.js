a52d7e32ecffba02b3437a6f6bd1e618c654f103
dargasova <dianaargasova@gmail.com>
2024-10-29 15:01:28 +0300

    Финальная версия фронтенда

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import App from './App';
-
-test('renders FBIISM Store header', () => {
-    render(<App/>);
-    const headerElement = screen.getByText(/FBIISM Store/i);
-    expect(headerElement).toBeInTheDocument();
-});
-
